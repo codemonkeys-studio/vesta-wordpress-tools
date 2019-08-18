665f2d7290f0b1680afda61311e63c7e0aec9324
Georgios Tsarmpopoulos <tsarbo@gmail.com>
2019-08-18 19:25:21 +0300

    add acheck for let's encrypt successful installation before redirecting

--- install.sh
+++ install.sh
@@ -12,24 +12,24 @@ NC='\033[0m' # No Color
 echo -e "${YELLOW}Moving files in all the right places!${NC}";
 echo -e "${GREEN}Installing the v-installwp script!${NC}";
 chmod +x v-installwp.sh
-mv v-installwp.sh /usr/local/bin/v-installwp
+cp v-installwp.sh /usr/local/bin/v-installwp
 echo -e "${BLUE}v-installwp installed!${NC}";
 
 echo -e "${GREEN}Installing the v-migratewp script!${NC}";
 chmod +x v-migratewp.sh
-mv v-migratewp.sh /usr/local/bin/v-migratewp
+cp v-migratewp.sh /usr/local/bin/v-migratewp
 echo -e "${BLUE}v-migratewp installed!${NC}";
 
 echo -e "${GREEN}Installing the v-fixwpssl script!${NC}";
 chmod +x v-fixwpssl.sh
-mv v-fixwpssl.sh /usr/local/bin/v-fixwpssl
+cp v-fixwpssl.sh /usr/local/bin/v-fixwpssl
 echo -e "${BLUE}v-fixwpssl installed!${NC}";
 
 echo -e "${GREEN}Installing the helper scripts!${NC}";
 chmod +x v-dropbox_list.phar
-mv v-dropbox_list.phar /usr/local/bin/v-dropbox_list
+cp v-dropbox_list.phar /usr/local/bin/v-dropbox_list
 chmod +x v-dbexists.phar
-mv v-dbexists.phar /usr/local/bin/v-dbexists
+cp v-dbexists.phar /usr/local/bin/v-dbexists
 echo -e "${BLUE}Helper scripts installed!${NC}";
 
 echo -e "${YELLOW}******${GREEN}All Done${YELLOW}******${NC}";
